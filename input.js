be053b9f037d3e4e89d2caddc45e9c568c7fbeca
Edison Yang <edison.cy.yang@gmail.com>
2019-12-04 21:54:26 +0000

    handled user wasd input to send move command to server

--- input.js
+++ input.js
@@ -1,8 +1,11 @@
+let connection;
+
 /**
  * Setup User Interface 
  * Specifically, so that we can handle user input via stdin
  */
-const setupInput = function() {
+const setupInput = function(conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
@@ -18,6 +21,18 @@ const handleUserInput = function(key) {
     process.stdout.write("Ending the game\n");
     process.exit();
   } 
+  if (key === "w") {
+    connection.write("Move: up");
+  }
+  if (key === "a") {
+    connection.write("Move: left");
+  }
+  if (key === "d") {
+    connection.write("Move: right");
+  }
+  if (key === "s") {
+    connection.write("Move: down");
+  }
 };
 
 module.exports = { setupInput };
--- play.js
+++ play.js
@@ -2,6 +2,6 @@ const { connect } = require('./client');
 const { setupInput } = require('./input');
 
 console.log("connecting....");
-connect();
+const conn = connect();
 
-setupInput();
+setupInput(conn);
